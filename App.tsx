367dc96518464a687a26389606f2897385a55e08
son <son@sonui-MacBookAir.local>
2022-03-03 16:07:02 +0900

    readme on set

--- App.tsx
+++ App.tsx
@@ -8,23 +8,40 @@
 
 import React from 'react';
 import type { Node } from 'react';
-import { View } from 'react-native';
-import styled from 'styled-components/native';
+import { ScrollView, Text, View } from 'react-native';
 import HomePage from './src/HomePage';
-
-const PageContainer = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-
-  background-color: #000;
-`;
 
 const App: () => Node = () => {
   return (
-    <PageContainer>
+    <ScrollView style={{ backgroundColor: '#000' }}>
       <HomePage />
-    </PageContainer>
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+      <HomePage />
+    </ScrollView>
   );
 };
 
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Text, View } from 'react-native';
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-const HomePageView = styled.View``;
+const HomePageView = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+interface TextProps {
+  color?: string;
+}
 
 const HomePageText = styled.Text`
   color: #fff;
   font-size: 20px;
+  ${(props: TextProps) =>
+    props.color &&
+    css`
+      color: ${props.color};
+    `}
 `;
 
 const HomePage = () => {
   return (
     <HomePageView>
-      <HomePageText>아주 잘되는 구만!</HomePageText>
+      <HomePageText color="#ff0">아주 잘되는 구만!</HomePageText>
+      <HomePageText color="#f00">구우웃</HomePageText>
     </HomePageView>
   );
 };
